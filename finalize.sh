e9568d58e8ed648eb3ffb4d1c0e75f485ac2578b
Mostafa Bagheri <mostafa@nxtrobotics.com>
2020-10-27 18:14:01 -0700

    updated files

--- finalize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-read -p "Comment: " cm
-git add -A
-git commit -am "$cm"
-git push origin master
--- start.sh
+++
@@ -1,4 +0,0 @@
-export GIT_SSL_NO_VERIFY=1
-git remote set-url origin git@git.ucsd.edu:mobagher/interview.git
-git pull
-echo "Last Pull on $(date)" > logfile.txt
